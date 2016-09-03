8fbd4e13f1d6872854d5bba2cb40e7bad5fc90af
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-09-04 02:54:21 +0800

    [app/proj] add a foreign key refering to Project for Graph

--- app/proj/models.py
+++ app/proj/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 
-class Graph(models.Model):
-    name = models.CharField(max_length=255)
-
-
 class Project(models.Model):
     name = models.CharField(max_length=255)
+
+
+class Graph(models.Model):
+    name = models.CharField(max_length=255)
+    proj = models.ForeignKey(Project)
