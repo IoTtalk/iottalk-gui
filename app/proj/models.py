9b5d651d5bc6443478fa393d5f8e4854f10cc4df
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-09-04 03:03:38 +0800

    [app/proj] Remove field `name` from `Graph`

--- app/proj/models.py
+++ app/proj/models.py
@@ -4,7 +4,12 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return '{} {}'.format(self.id, self.name)
+
 
 class Graph(models.Model):
-    name = models.CharField(max_length=255)
     proj = models.ForeignKey(Project)
+
+    def __str__(self):
+        return '{} in Proj {}'.format(self.id, self.proj.name)
