6b0a3a7f969c56b33776c249441b8d53126858ec
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-09-07 08:59:08 +0800

    [ui/MainNav] add hover menu for model

--- app/static/vue/MainNav.vue
+++ app/static/vue/MainNav.vue
@@ -7,9 +7,15 @@
       )
         i.home.icon
         | Home
-      a.item
+      .ui.pointing.dropdown.link.item
         i.cube.icon
         | Model
+        i.dropdown.icon
+        .menu
+          .item(
+            v-for="model in models"
+            v-on:click="onModelSelect(model)"
+          ) [[ model.name ]]
       a.item(
         v-link="{path: '/setting', activeClass: 'active'}"
       )
@@ -18,7 +24,37 @@
 </template>
 
 <script>
+import semantic from "../../static/semantic/semantic.min.js";
+
 export default {
+  data() {
+    return {
+      models: [],
+    };
+  },
+  methods: {
+    getModels() {
+      this.$http.get('/mod/').then(
+        res => {  // on success
+          this.models = res.json();
+        },
+        res => {  // on error
+          console.log('error: ' + res.json());
+        }
+      );
+    },
+    onModelSelect(model) {
+      console.log(model);
+    },
+  },
+  ready() {
+    this.getModels();
+    $('.ui.dropdown').dropdown({
+      on: 'hover',
+      transition: 'drop',
+      action: 'hide',
+    });
+  },
 }
 </script>
 
