bba33aa171a7846a8fa048c62b6fc4c1d91621d2
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-09-28 13:33:31 +0800

    [ui/svg] import svg modulde - Panel and ProgressBar

---
+++ app/static/vue/LineSVG.vue
@@ -0,0 +1,28 @@
+<template lang="jade">
+  svg
+    progress-bar
+</template>
+
+<script>
+import ProgressBar from './svg/ProgressBar.vue'
+
+export default {
+  components: {
+    ProgressBar,
+  },
+}
+</script>
+
+<style scoped>
+svg {
+  width: 50%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+</style>
+
+<style>
+</style>
---
+++ app/static/vue/svg/ProgressBar.vue
@@ -0,0 +1,31 @@
+<template lang="jade">
+    path(
+      d="M [[ startx ]] [[ starty ]] a 2 2 0 0 0 0 5 h 300 a 2 2 0 0 0 0 -5 Z"
+      stroke="#e5e5e5"
+      fill="#e5e5e5"
+      stroke-width="1"
+    )
+    path(
+      d="M [[ startx ]] [[ starty ]] a 2 2 0 0 0 0 5 h 100 a 2 2 0 0 0 0 -5 Z"
+      stroke="#888"
+      fill="#888"
+      stroke-width="1"
+    )
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      startx: '250',
+      starty: '100',
+    }
+  },
+}
+</script>
+
+<style>
+</style>
+
+<style scoped>
+</style>
